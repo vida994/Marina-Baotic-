04a090d54480e3340ad7a4ed6411e47ac5085ad4
vida994 <avidakovic14@gmail.com>
2021-03-21 14:08:03 +0100

    Commit

--- Marina Baotić/Managers/ContentfulManager.swift
+++ Marina Baotić/Managers/ContentfulManager.swift
@@ -30,38 +30,6 @@ class ContentfulManager {
                 
     }
     
-    func getGastroObjects(contentType:String, vc:UIViewController, completion: @escaping(Result<[Object], BaoticError>) -> Void) {
-        vc.showLoadingView()
-        var objects = [Object]()
-        ContentfulManager.shared.getEntries(contentType: contentType) { (result) in
-            vc.dismissLoadingView()
-            switch result {
-            case .success(let gastroObjects):
-                gastroObjects.forEach { (item) in
-                    let gastroObject = Object(
-                        headline: item.fields["headline"] as? String ?? "",
-                        openingHours: item.fields["openingHours"] as? String,
-                        mainImage: item.fields.linkedAsset(at: "mainImage")?.url ?? URL(string: "https://dl.dropbox.com/s/n3efhl7fa11f3wv/1603085475Marina-Baoti%C4%87-2020.jpg?dl=0")!,
-                        shortDescription: item.fields["shortDescription"] as? String ?? "",
-                        longDescription: item.fields["longDescription"] as? String ?? "",
-                        pricelist1: item.fields.linkedAsset(at: "pricelist1")?.url,
-                        pricelist2: item.fields.linkedAsset(at: "pricelist2")?.url,
-                        imageAsstets: item.fields.linkedAssets(at: "selecetionImages"),
-                        phoneNumber: item.fields["phoneNumber"] as? String ?? "+385912900057",
-                        email: item.fields["email"] as? String ?? "reception@marinabaotic.com")
-                    objects.append(gastroObject)
-                    completion(.success(objects))
-                }
-                
-                
-            case .failure(let error):
-                completion(.failure(error))
-                
-            }
-        }
-    }
-    
-    
     func getObjects(contentType:String, vc:UIViewController, completion: @escaping ([Object]?) -> Void){
         vc.showLoadingView()
         var fetchedObjects = [Object]()
